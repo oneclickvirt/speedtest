fd1c4391f4d59803d552410d6897ae088da20a1d
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-06-29 17:28:59 +0800

    Update sp.go

--- sp/sp.go
+++ sp/sp.go
@@ -20,7 +20,7 @@ var speedtestClient = speedtest.New(speedtest.WithUserConfig(
 	&speedtest.UserConfig{
 		UserAgent:      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.74 Safari/537.36",
 		PingMode:       speedtest.TCP,
-		MaxConnections: 1,
+		MaxConnections: 8,
 	}))
 
 func checkError(err error) {
