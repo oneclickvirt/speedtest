78f40a121d78b89097403785eaf11a4b65ea2a56
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2024-06-29 02:30:51 +0000

    update

--- cmd/main.go
+++ cmd/main.go
@@ -3,5 +3,6 @@ package main
 import "github.com/oneclickvirt/speedtest/sp"
 
 func main() {
-	sp.NearbySpeedTest()
+	sp.NearbySpeedTest("en")
+	sp.CustomSpeedTest("https://raw.githubusercontent.com/spiritLHLS/speedtest.net-CN-ID/main/CN_Telecom.csv", 2)
 }
--- sp/sp.go
+++ sp/sp.go
@@ -1,12 +1,18 @@
 package sp
 
 import (
+	"encoding/csv"
 	"fmt"
-	"github.com/showwin/speedtest-go/speedtest"
-	"github.com/showwin/speedtest-go/speedtest/transport"
+	"io"
 	"log"
+	"sort"
 	"strings"
 	"time"
+
+	"github.com/imroc/req/v3"
+	"github.com/oneclickvirt/speedtest/model"
+	"github.com/showwin/speedtest-go/speedtest"
+	"github.com/showwin/speedtest-go/speedtest/transport"
 )
 
 func checkError(err error) {
@@ -15,8 +21,14 @@ func checkError(err error) {
 	}
 }
 
-func NearbySpeedTest() {
-	fmt.Println("位置\t\t 上传速度\t 下载速度\t 延迟\t  丢包率")
+func NearbySpeedTest(language string) {
+	if language == "zh" {
+		fmt.Printf("%-12s\t %-11s\t %-11s\t %-11s\t %-12s\n",
+			"位置", "上传速度", "下载速度", "延迟", "丢包率")
+	} else if language == "en" {
+		fmt.Printf("%-12s\t %-11s\t %-11s\t %-11s\t %-12s\n",
+			"Location", "Upload Speed", "Download Speed", "Latency", "PacketLoss")
+	}
 	var speedtestClient = speedtest.New()
 	serverList, _ := speedtestClient.FetchServers()
 	targets, _ := serverList.FindServer([]int{})
@@ -41,10 +53,93 @@ func NearbySpeedTest() {
 			PacketLoss = strings.ReplaceAll(packetLoss.String(), "Packet Loss: ", "")
 		})
 		checkError(err)
-		fmt.Printf("%s\t\t %s\t %s\t %s\t  %s\n",
-			NearbyServer.Name, NearbyServer.ULSpeed, NearbyServer.DLSpeed, NearbyServer.Latency, PacketLoss)
+		fmt.Printf("%-12s\t %-11s\t %-11s\t %-11s\t %-12s\n",
+			//NearbyServer.Name,
+			"Speedtest.net",
+			fmt.Sprintf("%.2f Mbps", NearbyServer.ULSpeed.Mbps()),
+			fmt.Sprintf("%.2f Mbps", NearbyServer.DLSpeed.Mbps()),
+			NearbyServer.Latency,
+			PacketLoss)
 		NearbyServer.Context.Reset()
 	}
-	// https://github.com/spiritLHLS/speedtest.net-CN-ID
-	// https://github.com/spiritLHLS/speedtest.cn-CN-ID
+}
+
+func getData(endpoint string) string {
+	client := req.C()
+	client.SetTimeout(10 * time.Second)
+	client.R().
+		SetRetryCount(2).
+		SetRetryBackoffInterval(1*time.Second, 5*time.Second).
+		SetRetryFixedInterval(2 * time.Second)
+	for _, baseUrl := range model.CdnList {
+		url := baseUrl + endpoint
+		resp, err := client.R().Get(url)
+		if err == nil {
+			defer resp.Body.Close()
+			b, err := io.ReadAll(resp.Body)
+			if err == nil {
+				return string(b)
+			}
+		} else {
+			log.Println("Error accessing URL:", url, err)
+		}
+	}
+	return ""
+}
+
+func parseData(data string) speedtest.Servers {
+	var targets speedtest.Servers
+	reader := csv.NewReader(strings.NewReader(data))
+	reader.Comma = ','
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+	speedtestClient := speedtest.New()
+	for _, record := range records {
+		customURL := record[5] + ":" + record[6]
+		target, errFetch := speedtestClient.CustomServer(customURL)
+		if errFetch != nil {
+			continue
+		}
+		target.Name = record[3]
+		targets = append(targets, target)
+	}
+	return targets
+}
+
+func CustomSpeedTest(url string, num int) {
+	data := getData(url)
+	targets := parseData(data)
+	var pingList []time.Duration
+	var err error
+	serverMap := make(map[time.Duration]*speedtest.Server)
+	for _, server := range targets {
+		err = server.PingTest(nil)
+		checkError(err)
+		pingList = append(pingList, server.Latency)
+		serverMap[server.Latency] = server
+		server.Context.Reset()
+	}
+	sort.Slice(pingList, func(i, j int) bool {
+		return pingList[i] < pingList[j]
+	})
+	analyzer := speedtest.NewPacketLossAnalyzer(nil)
+	var PacketLoss string
+	for i := 0; i < num && i < len(pingList); i++ {
+		server := serverMap[pingList[i]]
+		server.DownloadTest()
+		server.UploadTest()
+		err = analyzer.Run(server.Host, func(packetLoss *transport.PLoss) {
+			PacketLoss = strings.ReplaceAll(packetLoss.String(), "Packet Loss: ", "")
+		})
+		checkError(err)
+		fmt.Printf("%-12s\t %-11s\t %-11s\t %-11s\t %-12s\n",
+			server.Name,
+			fmt.Sprintf("%.2f Mbps", server.ULSpeed.Mbps()),
+			fmt.Sprintf("%.2f Mbps", server.DLSpeed.Mbps()),
+			server.Latency,
+			PacketLoss)
+		server.Context.Reset()
+	}
 }
