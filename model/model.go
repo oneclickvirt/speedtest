a45cd982fa1f42989816f2e33d605fe97107f8aa
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-07-01 11:45:58 +0000

    fix: 添加权限校验

--- model/model.go
+++ model/model.go
@@ -1,6 +1,6 @@
 package model
 
-const SpeedTestVersion = "v0.0.9"
+const SpeedTestVersion = "v0.0.10"
 
 var EnableLoger = false
 var (
--- sp/sp.go
+++ sp/sp.go
@@ -22,7 +22,7 @@ func isSudoAvailable() bool {
 
 // 如果sudo可用，则使用sudo执行命令
 func execCommand(name string, arg ...string) *exec.Cmd {
-	if isSudoAvailable() {
+	if hasRootPermission() && isSudoAvailable() {
 		return exec.Command("sudo", append([]string{name}, arg...)...)
 	}
 	return exec.Command(name, arg...)
